222be1972978b5cd3d58b496e238ce437a9c68b8
Gabriel Coelho Góes <161400667+Cogah@users.noreply.github.com>
2025-04-15 08:22:34 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,7 @@
-
 public class App {
     public static void main(String[] args) {
+        Personagem cavaleiro = new Personagem("Cavaleiro", 100, 50, 30, 20, 10, 70);
+        Personagem mago = new Personagem("Mago", 80, 20, 60, 20, 100, 50);
         Jogador jogador1 = new Jogador(1, "Carlos", "1234");
         jogador1.cadastrar();
 
@@ -10,8 +11,9 @@ public class App {
             System.out.println("Falha na autenticação.");
         }
 
-        jogador1.adicionarPersonagem("Cavaleiro");
-        jogador1.adicionarPersonagem("Mago");
+
+        jogador1.adicionarPersonagem(cavaleiro);
+        jogador1.adicionarPersonagem(mago);
         jogador1.exibirPersonagens();
         jogador1.removerPersonagem();
         jogador1.exibirPersonagens();

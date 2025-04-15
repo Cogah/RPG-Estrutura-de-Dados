5cd3241753442bc1de8025dd9a9ffc664713d070
1drey2drey3drey <165610379+1drey2drey3drey@users.noreply.github.com>
2025-04-15 20:38:16 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,21 +1,12 @@
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
-        Personagem cavaleiro = new Personagem("Cavaleiro", 100, 50, 30, 20, 10, 70);
-        Personagem mago = new Personagem("Mago", 80, 20, 60, 20, 100, 50);
-        Jogador jogador1 = new Jogador(1, "Carlos", "1234");
-        jogador1.cadastrar();
 
-        if (jogador1.autenticar("1234")) {
-            System.out.println("Autenticação bem-sucedida!");
-        } else {
-            System.out.println("Falha na autenticação.");
-        }
+    public static void main(String[] args) throws IOException {
 
+        Menu menu = new Menu();
 
-        jogador1.adicionarPersonagem(cavaleiro);
-        jogador1.adicionarPersonagem(mago);
-        jogador1.exibirPersonagens();
-        jogador1.removerPersonagem();
-        jogador1.exibirPersonagens();
+        menu.menuInicio();
+        
     }
 }
